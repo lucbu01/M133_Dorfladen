c1a3a8bd79f53d93933f49e0ebc8b21e5edd030a
Luca Bucher <lucbu01@bluewin.ch>
2019-12-22 12:29:29 +0100

    get frontent on express server

--- lib/server.ts
+++ lib/server.ts
@@ -1,5 +1,44 @@
 import * as express from 'express';
+import * as fs from 'fs';
 
 const app = express();
+const port = 8080;
 
-app.listen(8080);
+app.get('/', (req, res) => {
+    res.header({'Content-Type': 'text/html', 'status': 200});
+    fs.readFile('frontend/dist/dorfladen/index.html', (err, data) => {
+        res.send(data);
+    });
+});
+
+app.get('/:path', (req, res) => {
+    fs.readFile(`frontend/dist/dorfladen/${req.params.path}`, (err, data) => {
+        if(err) {
+            res.header({'status': 404});
+            res.send();
+        } else {
+            res.header({'Content-Type': getContentType(req.params.path), 'status': 200});
+            res.send(data);
+        }
+    });
+});
+
+app.listen(port, () => console.log(`server is started and listen on port ${port}`));
+
+function getContentType(fileName: string): string {
+    if(fileName.endsWith('.html') || fileName.endsWith('.htm')){
+        return 'text/html';
+    }
+    if(fileName.endsWith('.js')){
+        return 'text/javascript';
+    }
+    if(fileName.endsWith('.jpg')){
+        return 'image/jpeg';
+    }
+    if(fileName.endsWith('.png')){
+        return 'image/png';
+    }
+    if(fileName.endsWith('.ico')){
+        return 'image/x-icon';
+    }
+}
